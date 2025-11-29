12e56ce79980cfaca897640238b5c5e6fc398572
Nytraveler <teraomedical058@gmail.com>
2025-11-29 10:40:28 +0900

    commit

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -3,4 +3,5 @@ class Prototype < ApplicationRecord
   has_many :comments, dependent: :destroy  # prototype削除時に関連コメントも削除
   has_one_attached :image
   validates :name, :catch_copy, :concept, presence: true
+  validates :image, presence: { message: 'を入力してください' }
 end
